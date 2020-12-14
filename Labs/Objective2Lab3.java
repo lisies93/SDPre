2c5187131598f2436cdcc6c96674a23fd11ccb28
lisies93 <75764587+lisies93@users.noreply.github.com>
2020-12-14 17:43:14 -0600

    Finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "Taco";
+    System.out.println(myVariable);
+    myVariable = "Hogdog";
+    System.out.println(myVariable);
+  }
+}
