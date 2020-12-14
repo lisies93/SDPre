3adb5e9ba5d0d51237d97729cf7f68d82004e94e
lisies93 <75764587+lisies93@users.noreply.github.com>
2020-12-14 17:41:43 -0600

    Finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+  public static void main(String[]args){
+    String name = "Karen Mena";
+    int age = 27;
+    String state = "Texas";
+
+    System.out.println("Name: " + " " + name +" " + "Age: " + " " + age +" "+ "State: " +" "+ state);
+  }
+}
