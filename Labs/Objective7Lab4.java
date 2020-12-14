e9b5c678fecbe8ad310aeced8420f13a36c2b3ef
lisies93 <75764587+lisies93@users.noreply.github.com>
2020-12-14 17:58:50 -0600

    Finish Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+
+int sum=0,counter=1;
+
+while(counter<=20){
+sum = sum + counter;
+  counter = counter + 1;
+}
+ System.out.println(sum);
+
+
+
+
+  }
+}
