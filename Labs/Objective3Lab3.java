fd1ec11dd360227a132347a23413f2d9ae34028e
lisies93 <75764587+lisies93@users.noreply.github.com>
2020-12-14 17:46:51 -0600

    Finish Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,8 @@
+public class Objective3Lab3{
+  public static void main(String[]args){
+int first= 1 + (int)(Math.random() * 6) ,second = 1 + (int)(Math.random() * 6),third= 1 + (int)(Math.random() * 6),
+sum = first+second+third;
+ System.out.println(first+"+"+second+"+"+third+"+"+"="+sum );
+
+  }
+}
